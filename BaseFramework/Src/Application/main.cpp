f30277bcd9ba5eb0cd6db33e67e16c83cd731b28
aokisou <kd1380646@st.kobedenshi.ac.jp>
2024-04-17 10:30:49 +0900

    テスト確認
    テスト用変数追加(消す)

--- BaseFramework/Src/Application/main.cpp
+++ BaseFramework/Src/Application/main.cpp
@@ -178,9 +178,9 @@ bool Application::Init(int w, int h)
 	// フルスクリーン確認
 	//===================================================================
 	bool bFullScreen = false;
-	if (MessageBoxA(m_window.GetWndHandle(), "フルスクリーンにしますか？", "確認", MB_YESNO | MB_ICONQUESTION | MB_DEFBUTTON2) == IDYES) {
-		bFullScreen = true;
-	}
+	//if (MessageBoxA(m_window.GetWndHandle(), "フルスクリーンにしますか？", "確認", MB_YESNO | MB_ICONQUESTION | MB_DEFBUTTON2) == IDYES) {
+	//	bFullScreen = true;
+	//}
 
 	//===================================================================
 	// Direct3D初期化
@@ -220,6 +220,9 @@ bool Application::Init(int w, int h)
 	// オーディオ初期化
 	//===================================================================
 	KdAudioManager::Instance().Init();
+
+	//テスト消す用
+	m_test = 10;
 
 	return true;
 }
